4eb7d1abba3f92b1f9eb68420b95431990b6aff1
Hazby <hazbyarp7@gmail.com>
2026-04-08 16:20:16 +0700

    chore: remove percentage table field and fix error

--- database/migrations/2026_03_14_085020_create_tasks_table.php
+++ database/migrations/2026_03_14_085020_create_tasks_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
             $table->string('category')
                 ->nullable()
                 ->comment("Default: strategy, development, planning, design, research, marketing, other");
-            $table->integer('progress_percentage')->default(0);
             $table->timestamps();
             $table->index(['user_id', 'status']);
         });
@@ -31,7 +30,7 @@ return new class extends Migration
      * Reverse the migrations.
      */
     public function down(): void
-    {d
+    {
         Schema::dropIfExists('tasks');
     }
 };
